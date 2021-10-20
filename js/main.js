540a80fef42d4524899d2cff600a9fedb37382b6
Guilherme <luizguilhermeh@yahoo.com.br>
2021-10-20 17:32:41 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*var nome = "Luiz Guilherme";
+var n1 = 5;
+var n2 = 6;
+var frase = "Brasil é a melhor seleção do mundo"
+//alert("Bem vindo " + nome)
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+
+var lista = ["maça", "pêra", "laranja"]
+//lista.push("uva");
+//lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));*/
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes);*/
+/*
+var count;
+for (count=0; count <= 5; count++) {
+    alert(count);
+};
+*/
+
+var count = 0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count++
+}
